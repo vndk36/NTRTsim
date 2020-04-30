49f7fd575d8ae003cb3f22aa4e45782e090675c2
vndk36 <victor.faraut@gmail.com>
2020-04-30 12:41:06 +0200

    Adding some comments

--- src/dev/vfaraut/AppSimpleTest.cpp
+++ src/dev/vfaraut/AppSimpleTest.cpp
@@ -9,7 +9,6 @@
 
 // This application
 #include "yamlbuilder/TensegrityModel.h"
-
 //include controller
 #include "SimpleController.h"
 // This library
@@ -25,12 +24,12 @@
 #include <string>
 #include <vector>
 
+// Forward declarations
 TensegrityModel* createModel(char const *userModelPath);
-tgSimulation* createSimulation(void);
 void createAndAttachedController(TensegrityModel* const myModel);
 
 /**
- * The entry point.
+ * Main function that creates model, simulation, and runs it 
  * @param[in] argc the number of command-line arguments
  * @param[in] argv argv[0] is the executable name
  * @param[in] argv argv[1] is the path of the YAML encoded structure
@@ -39,12 +38,26 @@ void createAndAttachedController(TensegrityModel* const myModel);
 
 int main(int argc, char const *argv[])
 {
-  
+    // Creates the model from the YAML file in the call of the App or with the 
+    // default file
     TensegrityModel* const myModel = createModel(argv[1]);
     
-    tgSimulation* simulation = createSimulation();
-    
-    std::cout << "Debug2"<< std::endl;
+    //tgSimulation* simulation = createSimulation();
+
+    // Creates a new ground object
+    tgBoxGround* ground = new tgBoxGround();
+
+    const tgWorld::Config config(98.1);
+    tgWorld world(config, ground); //degeu
+
+    // create the view
+    const double timestep_physics = 0.0001; // seconds
+    //const double timestep_physics = 0.001;
+    const double timestep_graphics = 1.f/60.f; // seconds
+    tgSimViewGraphics view(world, timestep_physics, timestep_graphics);
+    // create the simulation
+    tgSimulation* simulation = new tgSimulation(view);
+
     createAndAttachedController(myModel);
 
     simulation->addModel(myModel);
@@ -84,8 +97,13 @@ TensegrityModel* createModel(char const *userModelPath){
 
 /**
  * Function that creates the world, view and returns the linked tgSimulation.
+ ////////////////////////////////////////////////////////////////////////////
+ Function doesn't work due to the implementation of view. View is using world
+ directly as it is, and world is delete at the end of this function. The program
+ end up with seg-fault
+ ////////////////////////////////////////////////////////////////////////////
  * @return the simulation object to be run elsewhere
- */
+ 
 tgSimulation* createSimulation(void){
     const double yaw        = 0.0;
     const double pitch      = 0.0;
@@ -109,6 +127,7 @@ tgSimulation* createSimulation(void){
     std::cout << "Debug1"<< std::endl;
     return simulation;
 }
+*/
 
 /**
  * Function that creates the controller and attached it to the given model
@@ -126,6 +145,8 @@ void createAndAttachedController(TensegrityModel* const myModel){
   tagsToControl.push_back("m10");
   tagsToControl.push_back("m01");
   tagsToControl.push_back("m21");
+
+
   tagsToControl.push_back("m15");
   tagsToControl.push_back("m08");
   tagsToControl.push_back("m20");
